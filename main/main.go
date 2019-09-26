bbeaafb27b18cb630b297a6c60a1f6444d7b1f4f
melianor <ibrahimsn98@gmail.com>
2019-09-26 21:41:55 +0300

    add PoW

--- main/main.go
+++ main/main.go
@@ -1,58 +1,26 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
+	"blockchain/main/blockchain"
 	"fmt"
+	"strconv"
 )
 
-type BlockChain struct {
-	blocks []*Block
-}
-
-type Block struct {
-	Hash 		[]byte
-	Data 		[]byte
-	PrevHash 	[]byte
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
-	return block
-}
-
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, new)
-}
-
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
-}
-
 func main() {
-	chain := InitBlockChain()
+	chain := blockchain.InitBlockChain()
 
 	chain.AddBlock("First block after Genesis")
 	chain.AddBlock("Second block after Genesis")
 	chain.AddBlock("Third block after Genesis")
 
-	for _, block := range chain.blocks {
+	for _, block := range chain.Blocks {
 		fmt.Printf("Previus hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
 
